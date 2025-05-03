86899f00fcb04354cedd60ab3db8ee8b9b161b61
sxy <2712570987@qq.com>
2025-05-03 10:38:50 +0800

    remove old interface (ediotr_interface_2)

--- view/main_window.py
+++ view/main_window.py
@@ -17,7 +17,6 @@ from qfluentwidgets import FluentIcon as FIF
 # from test import MarkedCalendar
 from view.calendar_interface import CalendarInterface
 from view.editor_interface import EditorInterface
-from view.editor_interface_2 import EditorInterface2
 from view.editor_interface_3 import EditorInterface3
 from view.setting_interface import SettingInterface  # 导入 SettingInterface
 from dotenv import load_dotenv
@@ -30,7 +29,6 @@ class Main_Window(FluentWindow):
         # create sub interface
 
         self.editorInterface = EditorInterface(self)
-        self.editorInterface2 = EditorInterface2(self)
         self.editorInterface3 = EditorInterface3(self)
         self.calendarInterface = CalendarInterface(self)
         self.settingInterface = SettingInterface(self)  # 实例化 SettingInterface
@@ -58,29 +56,22 @@ class Main_Window(FluentWindow):
         # add sub interface
         # self.addSubInterface(self.focusInterface, FIF.RINGER, '专注时段')
         self.addSubInterface(
-            self.editorInterface, FIF.STOP_WATCH, "写日记editorInterface"
+            self.editorInterface, FIF.DOCUMENT, "写日记editorInterface"
         )
+
+        self.addSubInterface(self.editorInterface3, FIF.EDIT, "写日记editorInterface3")
+        self.addSubInterface(self.calendarInterface, FIF.CALENDAR, "日历")
         self.addSubInterface(
-            self.editorInterface2, FIF.STOP_WATCH, "写日记editorInterface2"
+            self.settingInterface,
+            FIF.SETTING,
+            "设置",
+            position=NavigationItemPosition.BOTTOM,
         )
-        self.addSubInterface(
-            self.editorInterface3, FIF.STOP_WATCH, "写日记editorInterface3"
-        )
-        self.addSubInterface(
-            self.calendarInterface, FIF.STOP_WATCH, "写日记editorInterface"
-        )
-        self.addSubInterface(self.settingInterface, FIF.SETTING, "设置")  # 添加设置界面
 
         self.navigationInterface.addWidget(
             routeKey="avatar",
             widget=NavigationAvatarWidget("zhiyiYo", "resource/images/shoko.png"),
             onClick=self.showMessageBox,
-            position=NavigationItemPosition.BOTTOM,
-        )
-        self.navigationInterface.addItem(
-            routeKey="settingInterface",
-            icon=FIF.SETTING,
-            text="设置",
             position=NavigationItemPosition.BOTTOM,
         )
 
